8740941535cd4ade842eed9955e8f7342eabddc9
lqi25 <55333782+lqi25@users.noreply.github.com>
2019-12-09 01:07:13 -0500

    Add files via upload

---
+++ seaborn.py
@@ -0,0 +1,6 @@
+import seaborn as sns
+sns.set()
+tips = sns.load_dataset("tips")
+sns.relplot(x="total_bill", y="tip", col="time",
+            hue="smoker", style="smoker", size="size",
+            data=tips);
